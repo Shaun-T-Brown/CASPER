2f724af3355ac9f9cda3cd6e98b74e595767af31
Shaun <shaunb866@outlook.com>
2021-10-15 16:36:26 +0100

    Added __all__ to the init file so the docstring documentation is available.

--- src/casper/__init__.py
+++ src/casper/__init__.py
@@ -1,4 +1,8 @@
-#automatically import casper on initialiasation so we don't have to multiply import
 
+__all__ =  ['Critical_density' , 'Einasto_density' , 'Einasto_mass' , 'casper', 'density_rms', 'peak_height', 
+            'smooth_kspace' , 'spherical_top_hat' , 'spherical_top_hat_generalised']
 from .casper import *
+
+
+
 
